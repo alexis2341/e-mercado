91a42ec123f72cbecd7bfc271cf01b752e44020f
alexis2341 <alexisvargas61@gmail.com>
2021-10-31 03:05:20 -0300

    porfile update

--- js/my-profile.js
+++ js/my-profile.js
@@ -4,3 +4,19 @@
 document.addEventListener("DOMContentLoaded", function (e) {
 
 });
+
+function infoUsuario(){
+    inputNombre = document.getElementById("nombre");
+    inputEdad = document.getElementById("edad");
+    inputTel  = document.getElementById("telefono");
+    inputEmail = document.getElementById("email");
+    localStorage.setItem("nombre", inputNombre.value);
+    localStorage.setItem("edad", inputEdad.value);
+    localStorage.setItem("telefono", inputTel.value);
+    localStorage.setItem("email", inputEmail.value);
+    location.reload();
+}
+document.getElementById("username").innerHTML = localStorage.getItem("nombre");
+document.getElementById("edadcard").innerHTML = localStorage.getItem("edad");
+document.getElementById("emailcard").innerHTML = localStorage.getItem("email");
+document.getElementById("telcard").innerHTML = localStorage.getItem("telefono");
